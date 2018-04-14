6a29f1a920c187a86cbefc5299645e41529e783e
tracychen112 <38146302+tracychen112@users.noreply.github.com>
2018-04-14 00:30:53 -0400

    Add files via upload

--- TP1.py
+++ TP1.py
@@ -5,10 +5,65 @@ quandl.ApiConfig.api_key = 'CQUhXPCW3sqs92KDd1rD'
 #data <- Quandl.datatable('MER/F1', compnumber="39102")
 
 
- 
 company = input("Enter stock symbol of a company: ")
 
+class Graph(object):
+    def __init__(self,company):
+        self.company = company 
+        self.dates = quandl.get('WIKI/'+company,rows=5)
+        
 
+
+class Solid(Graph):
+                
+            
+class CandleStick(Graph):
+    def __init__(self):
+        super().__init__(self)
+        self.open = quandl.get('WIKI/'+self.company,column_index=1)
+        self.high = quandl.get('WIKI/'+self.company,column_index=2)
+        self.low = quandl.get('WIKI/'+self.company,column_index=3)
+        # may delete this for inheritance 
+        self.close = quandl.get('WIKI/'+self.company,column_index=4)
+        
+    def getLowValues(self):
+        lowVal = []
+        for val in self.low:
+            for i in range(len(self.low[val])):
+                lowVal.append((self.low[val][i]))
+        return lowVal 
+        
+    def getHighValues(self):
+        highVal = []
+        for val in self.high:
+            for i in range(len(self.high[val])):
+                highVal.append((self.high[val][i]))
+        return highVal
+    
+        
+    def getOpenValues(self):
+        openVal = []
+        for val in self.open:
+            for i in range(len(self.open[val])):
+                openVal.append((self.open[val][i]))
+        return lowVal
+    
+    # from inheritance may delete this later 
+    def getClosingValues(self):
+        closeVal = []
+        for val in self.close:
+            for i in range(len(self.close[val])):
+                openVal.append((self.close[val][i]))
+        return closeVal
+    
+    def draw(self,canvas):
+        
+        
+            
+        
+    
+    
+"""
 def getDates():
     # Dates 
     dates = quandl.get('WIKI/'+company,rows = 5)
@@ -102,4 +157,72 @@ def runDrawing(width=300, height=300):
     root.mainloop()
     print("done!")
 
+"""
+
+## 112 website template 
+def init(data):
+    self.startState = True 
+    self.drawCandle = False 
+    self.drawSolidLine = False 
+    pass 
+
+def mousePressed(event, data):
+    pass 
+
+def redrawAll(canvas, data):
+    pass 
+
+def keyPressed(event, data):
+    pass
+
+def timerFired(data):
+    pass 
+
+####################################
+# use the run function as-is   
+####################################
+
+def run(width=300, height=300):
+    def redrawAllWrapper(canvas, data):
+        canvas.delete(ALL)
+        canvas.create_rectangle(0, 0, data.width, data.height,
+                                fill='white', width=0)
+        redrawAll(canvas, data)
+        canvas.update()    
+
+    def mousePressedWrapper(event, canvas, data):
+        mousePressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def keyPressedWrapper(event, canvas, data):
+        keyPressed(event, data)
+        redrawAllWrapper(canvas, data)
+
+    def timerFiredWrapper(canvas, data):
+        timerFired(data)
+        redrawAllWrapper(canvas, data)
+        # pause, then call timerFired again
+        canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
+    # Set up data and call init
+    class Struct(object): pass
+    data = Struct()
+    data.width = width
+    data.height = height
+    data.timerDelay = 100 # milliseconds
+    init(data)
+    # create the root and the canvas
+    root = Tk()
+    canvas = Canvas(root, width=data.width, height=data.height)
+    canvas.pack()
+    # set up events
+    root.bind("<Button-1>", lambda event:
+                            mousePressedWrapper(event, canvas, data))
+    root.bind("<Key>", lambda event:
+                            keyPressedWrapper(event, canvas, data))
+    timerFiredWrapper(canvas, data)
+    # and launch the app
+    root.mainloop()  # blocks until window is closed
+    print("bye!")
+
+run(400, 200)
 runDrawing(700, 600)
