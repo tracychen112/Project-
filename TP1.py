c8ebd8192caa0a14d49a1d9b205908af61dd05c9
tracychen112 <38146302+tracychen112@users.noreply.github.com>
2018-04-14 12:00:58 -0400

    Add files via upload

--- TP1.py
+++ TP1.py
@@ -52,8 +52,8 @@ class Graph(object):
         # print(closingVal)
         for val in self.close:
             for i in range(len(self.close[val])):
-                closeVal.append((self.close[val][i]))
-        #print (closeVal)
+                closeVal.append(self.close[val][i])
+        print (closeVal)
         return closeVal 
     
         #print(plotClosingVal)
@@ -94,8 +94,6 @@ class Solid(Graph):
         for i in range(len(dates)):
             xPos = i*increment + margin + startingPt
             yPos = yBase-yIncrement/scale* (closingValues[i]-minimum)
-            print(closingValues[i])
-            print (yPos)
             connectLines.append((xPos,yPos))
             canvas.create_line(xPos,self.height-margin,xPos,self.height-7*margin/8)
             # stack overflow for angle 
@@ -108,37 +106,97 @@ class Solid(Graph):
                 
             
 class CandleStick(Graph):
-    def __init__(self):
-        super().__init__(self)
-        self.open = quandl.get('WIKI/'+self.company,column_index=1)
-        self.high = quandl.get('WIKI/'+self.company,column_index=2)
-        self.low = quandl.get('WIKI/'+self.company,column_index=3)
-        
+    def __init__(self,company,width,height):
+        super().__init__(company,width,height)
+        self.open = quandl.get('WIKI/'+self.company,column_index=1,rows=5)
+        self.high = quandl.get('WIKI/'+self.company,column_index=2,rows=5)
+        self.low = quandl.get('WIKI/'+self.company,column_index=3,rows=5)
+        self.convertOpen = []
+        self.convertHigh = []
+        self.convertLow = []
+        self.convertClose = []
+        self.margin = self.width/10
+        self.startY = self.height-self.margin
+        self.startingPt = self.margin/2
+        self.yBase = self.startY-self.startingPt
+        self.minimum = None 
+        self.dates = self.getDates()
+        self.yIncrement = (self.height-2*self.margin)/len(self.dates)
+        # to get range:
+        self.minVal = 0
+        self.maxVal = 0 
+        scale = (self.maxVal-self.minVal)/len(self.dates)
+        self.scale = int(scale)+1
+        self.increment = (self.width-2*self.margin)/len(self.dates)
         
     def getLowValues(self):
         lowVal = []
         for val in self.low:
             for i in range(len(self.low[val])):
-                lowVal.append((self.low[val][i]))
-        return lowVal 
+                lowVal.append(self.low[val][i])
+        print(lowVal)
+        self.minVal = min(lowVal)
+        self.minimum = int(self.minVal)
+        print(self.minVal)
+        print(self.minimum)
+        for val in self.low:
+            for i in range(len(self.low[val])):
+                value = self.low[val][i]
+                newVal = self.yIncrement/self.scale * (value-self.minimum)
+                self.convertLow.append(newVal)
         
     def getHighValues(self):
         highVal = []
         for val in self.high:
             for i in range(len(self.high[val])):
-                highVal.append((self.high[val][i]))
-        return highVal
-    
+                highVal.append(self.high[val][i])
+        self.maxVal = max(highVal)
+        for val in self.high:
+            for i in range(len(self.high[val])):
+                value = self.high[val][i]
+                newVal = self.yIncrement/self.scale * (value-self.minimum)
+                self.convertHigh.append(newVal)
         
     def getOpenValues(self):
-        openVal = []
         for val in self.open:
             for i in range(len(self.open[val])):
-                openVal.append((self.open[val][i]))
-        return lowVal
+                value = self.open[val][i]
+                newVal = self.yIncrement/self.scale * (value-self.minimum)
+                self.convertOpen.append(newVal)
+
+    def getCloseValues(self):
+        for val in self.close:
+            for i in range(len(self.close[val])):
+                value = self.close[val][i]
+                newVal = self.yIncrement/self.scale * (value-self.minimum)
+                self.convertClose.append(newVal)
+        
     
     def drawCandleStick(self,canvas):
-        pass 
+    
+        canvas.create_rectangle(0,0,self.width,self.height,fill="light green")
+        canvas.create_rectangle(self.margin,self.margin,self.width-self.margin,self.height-self.margin,fill="white")
+        
+        for i in range(len(self.dates)):
+            xPos = i*self.increment + self.margin + self.startingPt
+            # high and low plot 
+            if self.convertClose[i]>self.convertOpen[i]:
+                bottom = self.convertClose[i]
+                top = self.convertOpen[i]
+            else:
+                bottom = self.convertOpen[i]
+                top = self.convertClose[i]
+            canvas.create_line(xPos,self.convertHigh[i],xPos,top)
+            canvas.create_line(xPos,self.convertLow[i],xPos,bottom)
+            canvas.create_line(xPos,self.height-self.margin,xPos,self.height-7*self.margin/8)
+            # stack overflow for angle 
+            canvas.create_text(xPos,self.height-30,text=self.dates[i],font="Calibri 8 bold",angle=90) 
+        
+        for i in range(len(self.dates)):
+            yPos = self.yBase-i*self.yIncrement
+            val = self.minimum + i*self.scale
+            canvas.create_line(7*self.margin/8,yPos,self.margin,yPos)
+            canvas.create_text(7*self.margin/8,yPos,text=str(val),anchor= E,font="Calibri 10 bold")
         
             
         
@@ -257,6 +315,10 @@ def redrawAll(canvas, data):
         Cover.draw(canvas,data.width,data.height)
     elif data.drawCandle:
         candle = CandleStick('FB',data.width,data.height)
+        candle.getLowValues()
+        candle.getHighValues()
+        candle.getOpenValues()
+        candle.getCloseValues()
         candle.drawCandleStick(canvas)
     elif data.drawSolidLine:
         line = Solid('FB',data.width,data.height)
@@ -265,7 +327,7 @@ def redrawAll(canvas, data):
 
 def keyPressed(event, data):
     if event.keysym=="c":
-        data.candleStick= True 
+        data.drawCandle= True 
         data.startState = False
         data.drawSolidLine = False
     elif event.keysym=="s":
