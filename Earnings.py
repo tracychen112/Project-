dc2d6e6bfeea94dcc45ecc99434dff717b95867e
tracychen112 <38146302+tracychen112@users.noreply.github.com>
2018-05-02 20:27:17 -0700

    Add files via upload

--- Earnings.py
+++ Earnings.py
@@ -3,123 +3,114 @@ import RBF
 import quandl 
 quandl.ApiConfig.api_key = 'CQUhXPCW3sqs92KDd1rD'
 
+# calculations of strategy/Portfolio page 
 
-# money left to invest, earnings/losses, total account--unsure of this one?
-def calculate(items,day,month,year):
+def calculate(items):
+    errorPresent = False 
     newLst = reorganizeLst(items)
-    print('here',newLst)
-   # print ('hi',newLst)
     overallEarn = 0
-    print('list',newLst)
     index =0 
     count=0
+    countError = 0
     for lst in newLst:
-        #print (lst)
-        #print('result',move(lst))
         if lst[0]!="" and lst[2]!="":
-            recentPrice = quandl.get('WIKI/'+lst[0],rows=1,column_index=4)
+            try:
+                recentPrice = quandl.get('WIKI/'+lst[0],rows=1,column_index=4)
+            except:
+                countError+=1
+                pass 
             for d in recentPrice:
                 recentPrice = list(recentPrice[d]) 
             items[3][index] = '%0.2f' % recentPrice[0] 
             items[4][index] = '%0.2f' % (recentPrice[0]*int(items[2][index]))  
         
-        earnings,futPrice = move(lst,day,month,year)
+        earnings,futPrice,errorPresent = move(lst)
+        if errorPresent:
+            countError+=1
         if earnings!=None and futPrice!=None:
             items[6][index]= "%0.2f" % futPrice
             items[7][index]= "%0.2f" % earnings
             overallEarn+=earnings 
+            print('overallEarn', overallEarn)
         else:
             count+=1
         index+=1
     if count==10:
         earnings = ''
+        errorPresent = False  
+    elif countError>1:
+        errorPresent = True
+        if overallEarn>0:
+            earnings = '%0.2d' % (overallEarn)
+        else:
+            earnings  = 0
     elif overallEarn==0:
         earnings='0'
+        errorPresent = False 
     else:
         earnings = '%0.2d' % (overallEarn)
-    return (items,earnings)
+        errorPresent = False 
+    print('count error',countError)
+    print('earnings',earnings)
+    return (items,earnings,errorPresent)
     
     
     
-    
-    
-    
-'''
-    year = date[2]
-    if len(date[0])==1:
-        month = '0'+date[0]
-    else:
-        month = date[0]
-    if len(date[1])==1:
-        day = '0'+date[1]
-    else:
-        day = date[1]
-    endDate = year+'-'+month+'-'+day
-'''
-            
+months = {'1':31,'2':28,'3':31,'4':30,'5':31,'6':30,'7':31,'8':31,'9':30,'10':31,'11':30,'12':31}
 
-def move(lst,day,month,year):
-    #print('date predetermined',Graphs.Portfolio.month,Graphs.Portfolio.day,Graphs.Portfolio.year)
-    if len(day)==1:
-        day = '0'+day
-    if len(month)==1:
-        month = '0'+month
-    start = year+'-'+month+'-'+day 
+def move(lst):
+    count = 0
+    for n in lst:
+        if n=='':
+            count+=1
+    if count==len(lst):
+        return (None,None,False)
     try:
-        #print('comp',lst[0])
+        futureDate = lst[5].split('/')
+        print  ('future Date', futureDate)
+        if len(futureDate)>3:
+            return (None,None,True)
+        else:
+            gapYear = int(futureDate[2])-2018
+            gapMonth = int(futureDate[0])-3        
+            if gapYear<0 or gapMonth<0:
+                return (None,None,True)
+            else:
+                gapDay = int(futureDate[1])-27
+                if gapDay<0 and gapMonth>0:
+                    gapDay = months[futureDate[0]]-27 + int(futureDate[1])
+                elif gapDay<0 and gapMonth<=0:
+                    return (None,None,True)
+        predDays = gapYear*365 + gapMonth*31 + gapDay 
+    except:
+        (None,None,True)
+    start = '2018-01-01'
+    try:
         data = quandl.get('WIKI/'+lst[0],start_date=start,end_date='2018-03-27',column_index=4)
         dates = quandl.get('WIKI/'+lst[0],start_date=start,end_date='2018-03-27')
-        #print(data)
-        #print ('company',lst[0])
-        #print ('start',date[0])
-        #print('end', date[1])
-        #print ('date: ',dates)
-        #dates2 = RegressionModel.getDates(dates)
+
         datelst = RegressionModel.getDates(dates)
         valLst = RegressionModel.getClosingValues(data)
-        predDays = 10
         rbf = RBF.mainPredict(datelst,valLst,predDays)
-        #print('here',rbfVal)
+        
         linVal = RegressionModel.linearReg(datelst,valLst,predDays)
         price = (rbf[1][-1]+linVal[-1])/2
         
         lastVal = quandl.get('WIKI/'+lst[0],rows=1)
-        # calculate buy here 
+        # calculate here
         currPrice = valLst[-1]
-        change =(currPrice-price) 
+        change =(price-currPrice) 
         shares = int(lst[2])
         result = shares*change
         if lst[1]=='SHORT':
             result=-result
-        return (result,price)
+        elif lst[1]!='BUY':
+            result = None  
+            return (result,price,True)
+        return (result,price,False)
     except:
-        return (None,None)
+        return (None,None,True)
 
-"""
-def dateRange():
-    now = Graph.endDate 
-    day = int(now.day)
-    year = int(now.year)
-    month = int(now.month)
-    # getting 15 days before for training
-    pastDay = int(day)-15
-    if pastDay<1: 
-        pastDay-=31-pastDay
-        month=int(month)-1
-        if month<1:
-            month =12-int(month)
-            year= int(year)-1
-    if len(str(month))==1:
-        month = '0'+str(month)
-    else:
-        month = str(month)
-    if len(str(pastDay))==1:
-        pastDay = '0'+str(pastDay)
-    else:
-        pastDay = str(pastDay)
-    startDate = str(year) + '-' +  month + '-' + pastDay
-    return (startDate,currDate)
-"""
     
 
 def reorganizeLst(items):
@@ -129,7 +120,5 @@ def reorganizeLst(items):
         for index in range(6):
             templst.append(items[index][i])
         itemlst.append(templst)
-    #print(itemlst)
     return itemlst
         
-#print(calculate)
